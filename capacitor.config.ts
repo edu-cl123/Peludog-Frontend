dcccb88b50b20645d7efb9f7539f83bdc568a9bf
unknown <educar200@gmail.com>
2024-07-15 18:25:54 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Peludog',
+  webDir: 'www'
+};
+
+export default config;
